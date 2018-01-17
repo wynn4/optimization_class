77f0ceafb367b17f531091cc7cef30aaeca08bff
Jesse Wynn <wynnsquad@gmail.com>
2018-01-17 13:25:49 -0700

    it runs, not confident about result

--- hw_1/spring_design.m
+++ hw_1/spring_design.m
@@ -1,9 +1,10 @@
 function [xopt, fopt, exitflag, output] = optimize_template()
 
     % ------------Starting point and bounds------------
-    x0 = [];
-    ub = [];
-    lb = [];
+    % design variables: d D n hf
+    x0 = [0.1, 0.5, 10.0, 1.5];   % starting point
+    ub = [0.25, 1.0, 50.0, 10.0];  % upper bound
+    lb = [0.05, 0.1, 1.0, 1.0];  % lower bound
 
     % ------------Linear constraints------------
     A = [];
@@ -15,6 +16,59 @@ function [xopt, fopt, exitflag, output] = optimize_template()
     function [f, c, ceq] = objcon(x)
         
         % set objective/constraints here
+        
+        % design variables (things we'll adjust to find optimum)
+        d = x(1);  % wire dia (in)
+        D = x(2);  % coil dia (in)
+        n = x(3);  % num coils
+        hf = x(4); % free height (no load) (in)
+        
+        % other analysis variables (constants that the optimization won't touch)
+        h0 = 1.0;           % preloaded height (in)
+        delta0 = 0.4;       % deflection (in)
+        hdef = h0 - delta0; % deflected spring height (in)
+        G = 12e6;
+        Q = 150e3;
+        w = 0.18;
+        Se = 45e3;
+        Sf = 1.5;
+        
+        % delta_x = 0.4;  % not sure if this is right??
+        delta_x = (hf - h0);  % maybe this instead???
+        
+        % analysis functions
+        k = G*d^4/(8*D^3*n);
+        F = k*delta_x;
+        K = ((4*D-d)/(4*(D-d)))+0.62*(d/D);
+        % Tau = (8*F*D/pi*d^3)*K;
+        hs = n*d;
+        F_min = k*(hf - h0);
+        F_max = F_min + delta0*k;
+        F_hs = k*(hf - hs);
+        Tau_min = (8*F_min*D/pi*d^3)*K;
+        Tau_max = (8*F_max*D/pi*d^3)*K;
+        Tau_m = (Tau_max + Tau_min)/2;
+        Tau_a = (Tau_max - Tau_min)/2;
+        Tau_hs = (8*F_hs*D/pi*d^3)*K;
+        Sy = 0.44*(Q/d^w);
+        
+        % objective function (what we're trying to optimize)
+        f = -F;  % maximize Force
+        
+        % inequality constraints (c<=0)
+        c = zeros(5,1);
+        c(1) = Tau_hs - Sy;
+        c(2) = Tau_a - Se/Sf;
+        c(3) = Tau_a + Tau_m - Sy/Sf;
+        c(4) = (D/d) - 16;
+        c(5) = -(D/d) + 4;
+        c(6) = d - 0.2;
+        c(7) = -d + 0.01;
+        c(8) = D + d - 0.75;
+        c(9) = -hdef + hs + 0.05;
+        
+        % equality constraints (ceq=0)
+        ceq = [];  % empty when we have none
 
     end
 
