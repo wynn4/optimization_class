b93673a0882c22f7e6c0873c9feb8e98345e271b
wynn4 <wynnsquad@gmail.com>
2018-02-02 23:22:26 -0700

    got steepest descent actually working correctly with the hw test function

--- hw_3/fminun.m
+++ hw_3/fminun.m
@@ -17,12 +17,10 @@ end
 % initialize variables
 alpha = alpha0;
 num_iterations = 0;
+max_iterations = 1e3;
 
-if n == 2
-    n = 2;
-elseif n == 3
-    n = 3;
-else
+
+if n < 2 || n > 3
     disp('Function Error: Number of variables must be either 2 or 3')
     xopt = [];
     fopt = [];
@@ -30,101 +28,135 @@ else
     return
 end
 
-
-% While the gradient is larger than zero...
-while (abs(grad(1,1)) > stoptol || abs(grad(2,1)) > stoptol)
-    
-    % start going along the function in the direction of s
-    xnew = x + alpha*s;
-    
-    % evaluate the function at xnew
-    fnew = obj(xnew);
-    
-    % if we're making progress going in the s direction...
-    if fnew < f
+if algoflag == 1  % steepest descent
+    while num_iterations < max_iterations
         
-        % double alpha and then keep going
-        alpha = alpha*2;
+        % start going along the function in the direction of s
+        xnew = x + alpha*s;
         
-        % set f equal to fnew and go again
-        f = fnew;
+        % evaluate the function at xnew
+        fnew = obj(xnew);
         
-    % Here we know we've gone too far.  Now we can conduct a 
-    % line search, find the approprate alpha, and start working
-    % in a new search direction.
-    elseif fnew > f
-        
-        a1 = alpha/4;
-        a2 = alpha/2;
-        a3 = alpha;
-        a4 = (a3 + a2)/2;
-        
-        % Now we need to pick the 3 points we will use
-        % to bracket the minimum and perform the quadratic
-        % line search.
-        
-        % evaluate the function at each
-        f1 = obj(x + a1*s);
-        f2 = obj(x + a2*s);
-        f3 = obj(x + a3*s);
-        f4 = obj(x + a4*s);
-        
-        % find the minimum point
-        [~, idx] = min([f1, f2, f3, f4]);
-        
-        if idx == 2
-            a1 = a1;
-            a2 = a2;
-            a3 = a4;
+        % if we're making progress going in the s direction...
+        if fnew < f
             
-            f1 = f1;
-            f2 = f2;
-            f3 = f4;
+            % double alpha and then keep going
+            alpha = alpha*2;
             
-        elseif idx == 4
-            a1 = a2;
-            a2 = a4;
-            a3 = a3;
+            % set f equal to fnew and go again
+            f = fnew;
             
-            f1 = f2;
-            f2 = f4;
-            f3 = f3;
+            % Here we know we've gone too far.  Now we can conduct a
+            % line search, find the approprate alpha, and start working
+            % in a new search direction.
+        elseif fnew > f
+            
+            a1 = alpha/4;
+            a2 = alpha/2;
+            a3 = alpha;
+            a4 = (a3 + a2)/2;
+            
+            % Now we need to pick the 3 points we will use
+            % to bracket the minimum and perform the quadratic
+            % line search.
+            
+            % evaluate the function at each
+            f1 = obj(x + a1*s);
+            f2 = obj(x + a2*s);
+            f3 = obj(x + a3*s);
+            f4 = obj(x + a4*s);
+            
+            % find the minimum point
+            [~, idx] = min([f1, f2, f3, f4]);
+            
+            if idx == 2
+                a1 = a1;
+                a2 = a2;
+                a3 = a4;
+                
+                f1 = f1;
+                f2 = f2;
+                f3 = f4;
+                
+            elseif idx == 4
+                a1 = a2;
+                a2 = a4;
+                a3 = a3;
+                
+                f1 = f2;
+                f2 = f4;
+                f3 = f3;
+                
+            elseif idx == 1
+                
+                % in this case we need to go one more step back in the
+                % past. we also need to compute these in reverse order.
+                
+                a3 = a2;
+                a2 = a1;
+                a1 = a1/2;
+                
+                
+                f3 = f2;
+                f2 = f1;
+                f1 = obj(x + a1*s);
+                
+            else
+                % this case shouldn't happen
+                disp('Theres an Error in your thinking')
+            end
+            
+            % find the 'optimal' alpha using a quadratic line search
+            a_star = qline_search(a1, f1, a2, f2, a3, f3);
+            
+            % set a new 'x' value using a_star and the current search dir
+            x = x + a_star*s;
+            
+            % evaluate the gradient at this new 'x' location
+            grad = gradobj(x);
+            
+            % find our new search direction to start going in
+            s = srchsd(grad);
+            
+            % reset alpha down to a small number
+            alpha = alpha0;
+            
+            % increment the iteration count
+            num_iterations = num_iterations + 1;
+            
+            % check to see if the gradient is close enough to zero
+            % i.e. we're at the optimum.
+            if norm(grad) < stoptol
+                break
+            end
+            
         else
-            disp('Theres an Error in your thinking')
+            % f isn't changing so we've reached an optimum?
+            break
         end
         
-        % find the 'optimal' alpha using a quadratic line search
-        a_star = qline_search(a1, f1, a2, f2, a3, f3);
         
-        % set a new 'x' value using a_star and the current search dir
-        x = x + a_star*s;
-        
-        % evaluate the gradient at this new 'x' location
-        grad = gradobj(x);
-        
-        % find our new search direction to start going in
-        s = srchsd(grad);
-        
-        % reset alpha down to a small number
-        alpha = alpha0;
-        
-        % increment the iteration count
-        num_iterations = num_iterations + 1;
-        
-        
-    else
-        % f isn't changing so we've reached an optimum?
-        break
     end
-    
-    
 end
+
+
+%%%%%%%%%%%%%%%%%
+% function exit %
+%%%%%%%%%%%%%%%%%
+
+if num_iterations >= max_iterations
+    max_iter_str = num2str(max_iterations);
+    err_str = strcat('Failed to converge to an optimum after ', max_iter_str, ' iterations.');
+    disp([newline, err_str])
+end
+
 
 
 num_iterations
 xopt = x;
 fopt = obj(x);
 exitflag = 0;
+
 end
 
 % get steepest descent search direction as a column vector
--- hw_3/fminunDriv.m
+++ hw_3/fminunDriv.m
@@ -1,11 +1,13 @@
 function [] = fminunDriv()
 %----------------Example Driver program for fminun------------------
     clear;
+    clc;
 
     global nobj ngrad
     nobj = 0; % counter for objective evaluations
     ngrad = 0.; % counter for gradient evaluations
-    x0 = [1.; 1.]; % starting point, set to be column vector
+    x0 = [10., 10., 10.]'; % [x1, x2, x3]' starting point, set to be column vector
+    % x0 = [10., 10.]'; % [x1, x2, x3]' starting point, set to be column vector
     algoflag = 1; % 1=steepest descent; 2=conjugate gradient; 3=BFGS quasi-Newton
     stoptol = 1.e-3; % stopping tolerance, all gradient elements must be < stoptol
 
@@ -24,16 +26,25 @@ end
  function [f] = obj(x)
     global nobj
     %example function
-    f = 12 + 6*x(1) - 5*x(2) + 4*x(1)^2 -2*x(1)*x(2) + 6*x(2)^2;
+    % f = 12 + 6*x(1) - 5*x(2) + 4*x(1)^2 -2*x(1)*x(2) + 6*x(2)^2;
+    
+    % hw function
+    f = 20 + 3*x(1) - 6*x(2) + 8*x(3) + 6*x(1)^2 - 2*x(1)*x(2) - x(1)*x(3) + x(2)^2 + 0.5*x(3)^2;
     nobj = nobj +1;
  end
 
 % get gradient as a column vector
  function [grad] = gradobj(x)
     global ngrad
-    %gradient for function above
-    grad(1,1) = 6 + 8*x(1) - 2*x(2);
-    grad(2,1) = -5 - 2*x(1) + 12*x(2);
+    % gradient for example function above
+    % grad(1,1) = 6 + 8*x(1) - 2*x(2);
+    % grad(2,1) = -5 - 2*x(1) + 12*x(2);
+    
+    % gradient for hw function above
+    grad(1,1) = 3 + 12*x(1) - 2*x(2) - x(3);
+    grad(2,1) = -6 - 2*x(1) + 2*x(2);
+    grad(3,1) = 8 - x(1) + x(3);
+    
     ngrad = ngrad + 1;
  end
 
