78804ef85df5544835210550fdb63859dfde5bf5
wynn4 <wynnsquad@gmail.com>
2018-02-22 20:56:36 -0700

    started complex step

---
+++ hw_4/OptimizeTruss_complex.m
@@ -0,0 +1,69 @@
+
+    % ------------Starting point and bounds------------
+    %design variables
+    x0 = [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]; %starting point (all areas = 5 in^2)
+    lb = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]; %lower bound
+    ub = [20, 20, 20, 20, 20, 20, 20, 20, 20, 20]; %upper bound
+    global nfun;
+    nfun = 0;
+
+    % ------------Linear constraints------------
+    A = [];
+    b = [];
+    Aeq = [];
+    beq = [];
+    
+    % ------------Call fmincon------------
+    tic;
+    options = optimoptions(@fmincon,'display','iter-detailed','Diagnostics','on');
+    [xopt, fopt, exitflag, output] = fmincon(@obj, x0, A, b, Aeq, beq, lb, ub, @con, options);  
+    eltime = toc;
+    eltime
+    xopt    %design variables at the minimum
+    fopt    %objective function value at the minumum
+    [f, c, ceq] = objcon(xopt);
+    c
+    nfun
+
+    % ------------Objective and Non-linear Constraints------------
+    function [f, c, ceq] = objcon(x)
+        global nfun;
+        
+        %get data for truss from Data.m file
+        Data
+        
+        % insert areas (design variables) into correct matrix
+        for i=1:nelem
+            Elem(i,3) = x(i);
+        end
+
+        % call Truss to get weight and stresses
+        [weight,stress] = Truss(ndof, nbc, nelem, E, dens, Node, force, bc, Elem);
+
+        %objective function
+        f = weight; %minimize weight
+        
+        %inequality constraints (c<=0)
+        c = zeros(20,1);         % create column vector
+%         for i=1:10
+%             c(i) = abs(stress(i))-25000; % check stress both pos and neg         
+%         end
+        % 20 separate constraints
+        for i=1:10
+            c(2*i-1) = stress(i) - 25000;
+            c(2*i) = -stress(i) - 25000;
+        end
+        
+        %equality constraints (ceq=0)
+        ceq = [];
+        nfun = nfun + 1;
+
+    end
+
+    % ------------Separate obj/con (do not change)------------
+    function [f] = obj(x) 
+        [f, ~, ~] = objcon(x);
+    end
+    function [c, ceq] = con(x) 
+        [~, c, ceq] = objcon(x);
+    end
