0bd334a318117951b6380f45b39258eb8253bc66
Jesse Wynn <wynnsquad@gmail.com>
2018-03-28 18:04:52 -0600

    added function

---
+++ genetic_project/get_amps.m
@@ -0,0 +1,13 @@
+function amps = get_amps(thrust, combination)
+
+switch combination
+    case 15
+        disp('fifteen')
+        amps = 15/2;
+    case 16
+        disp('sixteen')
+        amps = 16/2;
+end
+
+
+end
