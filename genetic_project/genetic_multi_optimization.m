ea71632bab022dc87efa16fd92e90092f425b820
wynn4 <wynnsquad@gmail.com>
2018-03-29 21:48:04 -0600

    started main genetic optimization file

---
+++ genetic_project/genetic_multi_optimization.m
@@ -0,0 +1,72 @@
+clc
+clear
+close all
+
+% Algorithm Tuning Params
+N = 10;              % Generation size
+n_gen = 10;          % Number of generations
+p_crossover = 0.3;   % Probability that crossover occurs
+p_mutation = 0.01;   % Probability that mutation occurs
+
+% Other Params
+n_genes = 6;         % Number of genes (design variables) per chromosome
+
+% ---------------------
+% Chromosome Definition
+% ---------------------
+% A chromosome for this problem is defined by 6 integer design variables:
+
+% Variable 1: Motor
+%     Values: 1, 2, 3, or 4 corresponding to four different brushles DC
+%             motors available from a manufacturer
+%
+% Variable 2: Propeller Blade
+%     Values: 1, 2, 3, 4, 5, or 6 corresponding to six available propeller
+%             blade profiles
+%
+% Variable 3: Number of Propeller Blades
+%     Values: 1, or 2 corresponding to 2 or 3 propeller blades per motor
+%
+% Variable 4: Number of LiPo Battery Cells
+%      Values: 1, 2, 3, 4, or 5 corresponding to 4, 6, 8, 10, or 12 lithium
+%              polymer battery cells (each nominally 3.7 volts)
+%
+% Variable 5: Number of Motors
+%     Values: 1, 2, or 3 corresponding to 4 (quadcopter), 6 (hexacopter),
+%             or 8 (octocopter) motors and arms
+%
+% Variable 6: Battery Capacity
+%     Values: 1, 2, 3, or 4 corresponding to 23, 22, 17, and 16 amp hour
+%             (Ah) capacities available from a battery manufacurer
+%
+% -------------------------
+% End Chromosome Definition
+% -------------------------
+
+% Memory Allocation
+generation = zeros(N, n_genes);
+
+% Randomly select the first generation
+% TODO: make sure that all designs in 1st generation are valid designs
+for i = 1:N
+    for j = 1:n_genes
+        switch j
+            case 1
+                generation(i,j) = randi(4);
+            case 2
+                generation(i,j) = randi(6);
+            case 3
+                generation(i,j) = randi(2);
+            case 4
+                generation(i,j) = randi(5);
+            case 5
+                generation(i,j) = randi(3);
+            case 6
+                generation(i,j) = randi(4);
+            otherwise
+                disp("Error. Number of genes does not match number of cases.")
+                return
+        end
+        
+    end
+end
--- genetic_project/testspace.m
+++ genetic_project/testspace.m
@@ -2,21 +2,33 @@ clc
 clear
 close all
 
-Thr = [7.06, 12.45, 19.91, 28.15, 40.99, 52.27, 65.41]';
-Amp = [1.4, 2.9, 5.4, 8.6, 14.7, 21.7, 28.0]';
+% Thr = [7.06, 12.45, 19.91, 28.15, 40.99, 52.27, 65.41]';
+% Amp = [1.4, 2.9, 5.4, 8.6, 14.7, 21.7, 28.0]';
+% 
+% A = [ones(length(Thr),1), Thr, Thr.^2, Thr.^3];
+% 
+% x = A \ Amp;
+% 
+% % original data
+% plot(Thr, Amp)
+% hold on
+% 
+% thr_data = min(Thr):0.1:max(Thr);
+% %thr_data = 0:0.1:35;
+% amp_predict = x(1) + x(2)*thr_data + x(3)*thr_data.^2 + ...
+%               x(4)*thr_data.^3;
+% 
+% plot(thr_data, amp_predict)
+% legend('data', 'predicted')
+global loops
 
-A = [ones(length(Thr),1), Thr, Thr.^2, Thr.^3];
-
-x = A \ Amp;
-
-% original data
-plot(Thr, Amp)
-hold on
-
-thr_data = min(Thr):0.1:max(Thr);
-%thr_data = 0:0.1:35;
-amp_predict = x(1) + x(2)*thr_data + x(3)*thr_data.^2 + ...
-              x(4)*thr_data.^3;
-
-plot(thr_data, amp_predict)
-legend('data', 'predicted')
+for i = 1:5
+    if i == 4
+        i = 1;
+    end
+    loops = loops + 1
+    
+    if loops > 50
+        break
+    end
+end
